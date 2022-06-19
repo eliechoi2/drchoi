4c1d9dddf45db5a87b82c3a00eb76d2af29c9e96
Elie Choi <100371808+elianachoi@users.noreply.github.com>
2022-06-19 15:31:04 -0400

    a

---
+++ animate.js
@@ -0,0 +1,21 @@
+
+function Services() {
+  location.href = "#services";
+  document.querySelector('#services').scrollIntoView({
+    behavior: 'smooth'
+  });
+};
+
+function Patients() {
+  location.href = "#patients";
+  document.querySelector('#patients').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+function Contact() {
+  location.href = "#contact";
+  document.querySelector('#contact').scrollIntoView({
+    behavior: 'smooth'
+  });
+};
